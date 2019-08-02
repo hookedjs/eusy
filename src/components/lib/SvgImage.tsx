a7232a9ed03ab98812b9e87e82f3e1de71f188a7
Brian Dombrowski <bdombro@gmail.com>
2019-08-02 14:22:54 -0400

    Removed SvgImage lib, because we are using inline SVGs now

--- src/components/lib/SvgImage.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * Note: There is a known bug with some svgs with comments. Please minify them first to avoid until
- * a fix is found. ref: https://github.com/vault-development/react-native-svg-uri/issues/150
- */
-export { default as SvgImage } from 'react-native-svg-uri-reborn';
