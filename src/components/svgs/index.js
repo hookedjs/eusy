ce1c5dc921e88b1614590864c67b5764c0272433
Brian Dombrowski <bdombro@gmail.com>
2019-08-06 13:35:14 -0400

    Added back svgs indexjs

---
+++ src/components/svgs/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// created from 'create-ts-index'
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+__export(require("./ArrowLeft.icon"));
+__export(require("./ArrowRight.icon"));
+__export(require("./Lock.icon"));
+__export(require("./Logo.icon"));
+__export(require("./LogoCircle.icon"));
+__export(require("./LogoIcon.icon"));
+__export(require("./LogoIconCircle.icon"));
+__export(require("./Star.icon"));
