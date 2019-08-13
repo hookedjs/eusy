d4510df04fa5f915d1bdb61d4e54e4785522f894
Brian Dombrowski <bdombro@gmail.com>
2019-08-13 08:50:56 -0400

    Cleanup and enhanced logo

--- src/components/modules/Logo.module.tsx
+++ src/components/modules/Logo.module.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { WindowState } from '../../state/Window.state';
-import { LogoCircleIcon } from '../svgs';
+import { LogoIconSvg } from '../svgs/LogoIcon.svg';
 import { SvgProps } from 'react-native-svg';
 
 // Leaving props type as any until we get an SVG logo, then we'll switch it to svg.
 export const LogoModule = ({ width, height, ...props }: Partial<SvgProps>) => {
   return (
-    <LogoCircleIcon
+    <LogoIconSvg
       width={width || (WindowState.isLarge ? 200 : 120)}
       height={height || (WindowState.isLarge ? 200 : 120)}
       {...props}
--- src/components/svgs/ArrowRight.icon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Svg, { SvgProps, Path } from 'react-native-svg';
-
-export const ArrowRightIcon = (svgProps: SvgProps) => (
-  <Svg viewBox="0 0 24 24" {...svgProps}>
-    <Path d="m0 0h24v24h-24z" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-    <Path d="m10.46 18a2.23 2.23 0 0 1 -.91-.2 1.76 1.76 0 0 1 -1.05-1.59v-8.42a1.76 1.76 0 0 1 1.05-1.59 2.1 2.1 0 0 1 2.21.26l5.1 4.21a1.7 1.7 0 0 1 0 2.66l-5.1 4.21a2.06 2.06 0 0 1 -1.3.46z" />
-  </Svg>
-);
